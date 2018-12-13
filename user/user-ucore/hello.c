b1b49b05e742dcbcb4a978eacce59b684059af68
kedinie <2379074979@qq.com>
2018-12-13 17:18:49 +0800

    Initial commit

---
+++ user/user-ucore/hello.c
@@ -0,0 +1,16 @@
+#include <ulib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dir.h>
+#include <file.h>
+#include <stat.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#define printf(...)                     fprintf(1, __VA_ARGS__)
+
+int main(){
+    // printf("kernel: hello!\n");
+    printf("The return of hello is %d\n", hello());
+    return 0;
+}
